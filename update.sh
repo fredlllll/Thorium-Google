6fb396b3daafb9db585bd20f921215833ee9f109
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-17 17:26:11 +0100

    referencing projects intstead of dll

--- update.sh
+++ update.sh
@@ -6,6 +6,9 @@ git fetch
 git pull
 git submodule update --init --recursive
 git submodule update --recursive --remote
+cd Thorium-Computing-Farm/Source
+nuget restore
+cd ../..
 nuget restore
 msbuild
 EOM
